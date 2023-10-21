035bf166041ce2511c5fadcd8a84d2eeb30bc7cd
maheshinfotech <maheshinfotech01@gmail.com>
2023-10-21 17:15:41 +0530

    add migration

--- database/migrations/2023_10_21_171435_create_room_categories_table.php
+++ database/migrations/2023_10_21_171435_create_room_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AlterUsersAddColumnsRoleId extends Migration
+class CreateRoomCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class AlterUsersAddColumnsRoleId extends Migration
      */
     public function up()
     {
-        Schema::table('users',  function (Blueprint $table) {
-            $table->integer('role_id');
+        Schema::create('room_categories', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -25,8 +26,6 @@ class AlterUsersAddColumnsRoleId extends Migration
      */
     public function down()
     {
-        Schema::table('users',  function (Blueprint $table) {
-            $table->dropColumnIfExists('role_id');
-        });
+        Schema::dropIfExists('room_categories');
     }
 }
