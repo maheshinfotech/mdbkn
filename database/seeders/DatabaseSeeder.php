71572a685367a58547f355afa81c45e960a3905c
maheshinfotech <maheshinfotech01@gmail.com>
2023-10-23 11:37:13 +0530

    add seeder files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+
+
+     DB::insert( DB::raw(
+       " INSERT INTO `menus` (`id`, `menu_name`, `menu_href`, `menu_parent`, `menu_icon`, `menu_placeholder`, `menu_permissions`, `is_active`, `created_at`, `updated_at`, `deleted_at`, `is_section`, `order`) VALUES
+        (1, 'dashboard', 'dashboard', 0, '<i class=\"fa-solid fa-gauge-high\"></i>', 'dashboard', '1', 1, NULL, NULL, NULL, 0, 1),
+        (2, 'user', 'user', 0, '<i class=\"fa-solid fa-users\"></i>', 'User', '1,2,3,4\r\n', 1, NULL, NULL, NULL, 0, 1),
+        (3, 'rooms', 'rooms', 0, '<i class=\"fa-solid fa-person-shelter\"></i>', 'Rooms', '1,2,3', 1, NULL, NULL, NULL, 0, 1),
+        (4, 'booking', 'bookings', 0, '<i class=\"fa-solid fa-person-shelter\"></i>', 'Bookings', '1,2,3,4', 1, NULL, NULL, NULL, 0, 1);"
+
+     ));
+
+        \App\Models\User::Insert([
+            'name' => 'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',  //password
+            'is_active'=>1,
+            'role_id'=>1
+        ]);
+        $this->call([
+            RoomCategoriesSeeder::class,
+            RoomSeeder::class
+        ]);
     }
 }
