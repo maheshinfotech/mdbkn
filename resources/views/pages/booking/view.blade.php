9bca5e4fa5a96c37953ef56678f4f7dd960466d6
bhanu <msiplbhanu1@gmail.com>
2023-10-30 14:29:20 +0530

    done

--- resources/views/pages/booking/view.blade.php
+++ resources/views/pages/booking/view.blade.php
@@ -79,48 +79,7 @@
                 </div>
                 <!--card body ends -->
             </div>
-          <!--card body starts -->
-          <div class="card-body">
-              <div class="table-responsive">
-                  <table class="table table-striped align-middle py-3 text-center" id="booking_table" style="width:100%;white-space:nowrap;"
-                  data-paging="true" data-searching="true" data-ordering="false" data-info="false"
-                  >
-                      <thead>
-                          <tr>
-                              <th class="text-start">Guest Name</th>
-                              <th class="text-center">Patient Name</th>
-                              <th class="text-center">Check In Time</th>
-                              <th class="text-center">Check Out Time</th>
-                              <th class="text-center">Doctor Name</th>
-                              <th class="text-center">Mobile No.</th>
-                              <th class="text-center">Total Paid Amount</th>
-                              <th class="text-end">Action</th>
-                          </tr>
-                      </thead>
-                      <tbody class="text-capitalize">
-                        @foreach ($bookings as $booking)
-                          <tr>
-                              <td class="text-start">{{$booking->guest_name}}</td>
-                              <td>{{ $booking->patient_name }}</td>
-                              <td>{{ $booking->check_in_time ? : '--'}}</td>
-                              <td>{{ $booking->check_out_time ? : '--' }}</td>
-                              <td>{{ $booking->docter_name }}</td>
-                              <td>{{ $booking->mobile_number }}</td>
-                              <td>{{ $booking->paid_rent + $booking->advance_payment}}</td>
-                              <td class="text-end">
-                                <a href="{{ route('advance.create', ['booking_id' => $booking->id])}}" class="btn btn-sm btn-purple open-modal"><i class="fa-solid fa-plus"></i></a>
-                                @if ($booking->getRawOriginal('check_out_time')==null)
-                                <a href="/bookings/checkout/{{ $booking->id }}" class="btn btn-sm btn-purple">Checkout</a>
-                                @endif
-                                  <a href="/bookings/{{ $booking->id }}" class="btn btn-sm btn-purple">View </a>
-                              </td>
-                          </tr>
-                          @endforeach
-                      </tbody>
-                  </table>
-              </div>
-          </div>
-          <!--card body ends -->
+
         </div>
         <!-- container ends -->
     </div>
