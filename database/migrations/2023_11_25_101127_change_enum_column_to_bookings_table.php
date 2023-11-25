01f552e944d4438f72f72a73b70d353550cb780d
bhanu <msiplbhanu1@gmail.com>
2023-11-25 10:55:21 +0530

    enum change

---
+++ database/migrations/2023_11_25_101127_change_enum_column_to_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeEnumColumnToBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            \DB::statement("ALTER TABLE `bookings` CHANGE `ward_type` `ward_type` ENUM('ct','rt', 'oward', 'cancer','report','other') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci  NULL;");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
+    }
+}
