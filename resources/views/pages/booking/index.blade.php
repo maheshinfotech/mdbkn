f2180d1431c406df1bdd9a2a1065caa8b4e3d14e
ritik7862 <batrahritik83@gmail.com>
2023-11-29 15:41:04 +0530

    pullcode

--- resources/views/pages/booking/index.blade.php
+++ resources/views/pages/booking/index.blade.php
@@ -26,7 +26,7 @@
     <div class="">
         <a href="/dashboard" class="btn btn-lg btn-purple "> <i class="fa fa-arrow-left"></i> Back</a>
     </div>
-   
+
     <div class="">
         <h1 class="text-purple text-center my-3"> Current Bookings</h1>
     </div>
